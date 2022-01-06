c14bb62de469504de7e5e0f1d0375b0a30bc493b
folt-a <faultun@gmail.com>
2022-01-06 19:32:28 +0900

    kousin

--- composite_layer.sh
+++ composite_layer.sh
@@ -2,13 +2,13 @@
 
 # 重ねるやつとりあえず作成
 
-composite spriteSheet_fuku.png spriteSheet_bg.png merged.png
-composite spriteSheet_karada.png merged.png merged.png
-composite spriteSheet_kami.png merged.png merged.png
+composite sheets/spriteSheet_fuku.png sheets/spriteSheet_bg.png sheets/merged.png
+composite sheets/spriteSheet_karada.png sheets/merged.png sheets/merged.png
+composite sheets/spriteSheet_kami.png sheets/merged.png sheets/merged.png
 
-; エフェクトは加算発光50%で重ねたい
-; composite spriteSheet_effect.png merged.png merged.png
+# エフェクトは加算発光50%で重ねたい
+# composite spriteSheet_effect.png merged.png merged.png
 
-; composite -alpha on -evaluate set 50% spriteSheet_effect.png merged.png merged.png
+# composite -alpha on -evaluate set 50% spriteSheet_effect.png merged.png merged.png
 
-; magick layer1 layer2 ... layer5 ( -clone 0--1 -background none -flatten ) +insert result.psd
+# magick layer1 layer2 ... layer5 ( -clone 0--1 -background none -flatten ) +insert result.psd
